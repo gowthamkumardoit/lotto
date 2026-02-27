ac7701f4886d1332cdf55039c76113755d495e57
Gowtham Kumar <gowthamkumardoit@gmail.com>
2026-02-27 16:26:18 +0530

    Added functionalities related to Kuber Gold

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 /**
- * Merge Tailwind + conditional classNames safely
+ * Merge Tailwind + conditional classNames safely add new comment
  */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
